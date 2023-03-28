a5f98fc6b6b50525f611afa3cc6ea3837258f36a
jatin <it1806@global.org.in>
2023-03-28 22:14:45 +0530

    added change password and user validation

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,16 @@ const {
   logout,
   forgotPassword,
   passwordReset,
+  getLoggedInUserDetails,
+  changePassword,
 } = require("../controllers/userController");
+const { isLoggedIn } = require("../middlewares/user");
 router.route("/signup").post(signup);
 router.route("/login").post(login);
-router.route("/logout").post(logout);
+router.route("/logout").get(logout);
 router.route("/forgotPassword").post(forgotPassword);
 router.route("/password/reset/:token").post(passwordReset);
+router.route("/password/update").post(isLoggedIn, changePassword);
+router.route("/userdashboard").get(isLoggedIn, getLoggedInUserDetails);
 
 module.exports = router;
