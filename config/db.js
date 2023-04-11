1997662627626f50825872e8b9f403f0e9c0ed03
Jatinchourasia <jatinchourasia3@gmail.com>
2023-04-11 20:02:39 +0530

    updated docs

--- config/db.js
+++ config/db.js
@@ -1,19 +1,4 @@
 const mongoose = require("mongoose");
-
-// const connectWithDb = () => {
-//   mongoose
-//     .connect(process.env.DB_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then(console.log(`DB GOT CONNECTED`))
-//     .catch((error) => {
-//       console.log(`DB CONNECTION ISSUES`);
-//       console.log(error);
-//       process.exit(1);
-//     });
-// };
-
 const connectWithDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_URL, {
