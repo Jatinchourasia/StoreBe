d2fef23ac82e411c5e8aa1c1e6231d413a99d4e9
Jatinchourasia <jatinchourasia3@gmail.com>
2023-04-13 23:16:16 +0530

    added review feature

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,6 +11,128 @@ exports.product = BigPromise(async (req, res) => {
   });
 });
 
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new CustomError("no product found wih this id", 401));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+exports.getAllProduct = BigPromise(async (req, res, next) => {
+  const resultPerPage = 6;
+  const totalcountProduct = await Product.countDocuments();
+  const productsObj = new WhereClause(Product.find(), req.query)
+    .search()
+    .filterAggregation();
+  let products = await productsObj.base;
+  const filterProductNumber = products.length;
+  //products.limit().skip()
+  productsObj.pager(resultPerPage);
+  // await will not give the res need to chain .clone() to run multiple methods
+  products = await productsObj.base.clone();
+  res.status(200).json({
+    success: true,
+    products,
+    filterProductNumber,
+    totalcountProduct,
+  });
+});
+//reviews
+exports.addReview = BigPromise(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const AlreadyReview = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (AlreadyReview) {
+    product.reviews.forEach((review) => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numberOfReviews = product.reviews.length;
+  }
+
+  // adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //save
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.deleteReview = BigPromise(async (req, res, next) => {
+  const { productId } = req.query;
+
+  const product = await Product.findById(productId);
+
+  const reviews = product.reviews.filter(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  const numberOfReviews = reviews.length;
+
+  // adjust ratings
+
+  product.ratings =
+    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    product.reviews.length;
+
+  //update the product
+
+  await Product.findByIdAndUpdate(
+    productId,
+    {
+      reviews,
+      ratings,
+      numberOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+exports.getOnlyReviewsForOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+//admin routes
 exports.addProduct = BigPromise(async (req, res, next) => {
   console.log("res", req.body);
 
@@ -49,43 +171,11 @@ exports.addProduct = BigPromise(async (req, res, next) => {
   });
 });
 
-exports.getAllProduct = BigPromise(async (req, res, next) => {
-  const resultPerPage = 6;
-  const totalcountProduct = await Product.countDocuments();
-  const productsObj = new WhereClause(Product.find(), req.query)
-    .search()
-    .filterAggregation();
-  let products = await productsObj.base;
-  const filterProductNumber = products.length;
-  //products.limit().skip()
-  productsObj.pager(resultPerPage);
-  // await will not give the res need to chain .clone() to run multiple methods
-  products = await productsObj.base.clone();
-  res.status(200).json({
-    success: true,
-    products,
-    filterProductNumber,
-    totalcountProduct,
-  });
-});
-
 exports.adminGetAllProducts = BigPromise(async (req, res, next) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
-  });
-});
-
-exports.getOneProduct = BigPromise(async (req, res, next) => {
-  const product = await Product.findById(req.params.id);
-  if (!product) {
-    return next(new CustomError("no product found wih this id", 401));
-  }
-
-  res.status(200).json({
-    success: true,
-    product,
   });
 });
 
